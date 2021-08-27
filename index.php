2345cbb0c907aeb6edc8578fb42a976b70d191d6
midiatecdf <89493698+midiatecdf@users.noreply.github.com>
2021-08-27 20:40:54 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,11 +2,52 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Loja Doces</title>
+<title>Loja de Doces</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 
 <body>
-	<head>
-	</head>
+<header id="Header">
+	
+	<table width="100%" border="0">
+  <tbody>
+    <tr>
+      <td><img src="images/logo-1.png" width="198" height="86" alt=""/></td>
+      <td><center><a href="index.php">Home</a></center></td>
+      <td><center><a href="index.php?pagina=pages/quem-somos.php">Quem Somos</a></center></td>
+      <td><center><a href="index.php?pagina=pages/contatos.php">Contatos</a></center></td>
+      
+    </tr>
+  </tbody>
+</table>
+
+	
+	</header>
+	
+<main id="Main">
+	
+	<?php
+    if (isset($_GET["pagina"]) && !empty($_GET["pagina"])) {
+        $pagina = $_GET["pagina"];
+        include ($pagina);
+    } else {
+         include ("pages/produtos.php");
+         //echo "Principal";
+    }
+    ?>
+	</main>
+	
+<footer id="Footer">
+	<table width="100%" border="0">
+  <tbody>
+    <tr>
+      <td><center>RA: 1224213-5</center></td>
+      <td><center>Erotides Caetano Júnior</center></td>
+      <td><center>Superior de Tecnologia em Sistemas de Internet</center></td>
+    </tr>
+  </tbody>
+</table>
+
+	</footer>
 </body>
 </html>
